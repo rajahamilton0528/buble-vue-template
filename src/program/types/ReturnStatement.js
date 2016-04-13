rajahamilton0528/buble-vue-template

commit 8d4a40e5a0ec73f11f7413f80b76e5845709d5fa

support continuing and breaking from loops rewritten as functions (#12)

src/program/types/ReturnStatement.js
@@ -1,0 +1,15 @@
+import Node from '../Node.js';
+
+export default class ReturnStatement extends Node {
+	initialise () {
+		this.loop = this.findNearest( /(?:For|While)Statement/ );
+		this.loop.canReturn = true;
+	}
+
+	transpile ( code ) {
+		if ( this.loop.shouldRewriteAsFunction ) {
+			code.insert( this.argument.start, `{ v: ` );
+			code.insert( this.argument.end, ` }` );
+		}
+	}
+}

src/program/types/BreakStatement.js
@@ -1,0 +1,16 @@
+import Node from '../Node.js';
+import CompileError from '../../utils/CompileError.js';
+
+export default class BreakStatement extends Node {
+	initialise () {
+		this.loop = this.findNearest( /(?:For|While)Statement/ );
+		this.loop.canBreak = true;
+	}
+
+	transpile ( code ) {
+		if ( this.loop.shouldRewriteAsFunction ) {
+			if ( this.label ) throw new CompileError( this, 'Labels are not currently supported in a loop with locally-scoped variables' );
+			code.overwrite( this.start, this.start + 5, `return 'break'` );
+		}
+	}
+}

src/program/types/ContinueStatement.js
@@ -1,0 +1,12 @@
+import Node from '../Node.js';
+import CompileError from '../../utils/CompileError.js';
+
+export default class ContinueStatement extends Node {
+	transpile ( code ) {
+		const loop = this.findNearest( /(?:For|While)Statement/ );
+		if ( loop.shouldRewriteAsFunction ) {
+			if ( this.label ) throw new CompileError( this, 'Labels are not currently supported in a loop with locally-scoped variables' );
+			code.overwrite( this.start, this.start + 8, 'return' );
+		}
+	}
+}

src/program/types/ForStatement.js
@@ -42,8 +42,8 @@ export default class ForStatement extends Node {
 
 	transpile ( code ) {
 		if ( this.shouldRewriteAsFunction ) {
-			const indentation = this.getIndentation();
-			const indentString = code.getIndentString();
+			const i0 = this.getIndentation();
+			const i1 = i0 + code.getIndentString();
 
 			// which variables are declared in the init statement?
 			const names = this.init.type === 'VariableDeclaration' ?
@@ -63,18 +63,32 @@ export default class ForStatement extends Node {
 					code.insert( this.body.body[0].end, `; ${updates.join(` `)}` );
 				} else {
 					const lastStatement = this.body.body[ this.body.body.length - 1 ];
-					code.insert( lastStatement.end, `\n\n${indentation}${indentString}${updates.join(`\n${indentation}${indentString}`)}` );
+					code.insert( lastStatement.end, `\n\n${i1}${updates.join(`\n${i1}`)}` );
 				}
 			}
 
-			const before = `var forLoop = function ( ${params.join( ', ' )} ) ` + ( this.body.synthetic ? `{\n${indentation}${code.getIndentString()}` : '' );
-			const after = ( this.body.synthetic ? `\n${indentation}}` : '' ) + `;\n\n${indentation}`;
+			const functionScope = this.findScope( true );
+			const loop = functionScope.createIdentifier( 'loop' );
+
+			const before = `var ${loop} = function ( ${params.join( ', ' )} ) ` + ( this.body.synthetic ? `{\n${i0}${code.getIndentString()}` : '' );
+			const after = ( this.body.synthetic ? `\n${i0}}` : '' ) + `;\n\n${i0}`;
 
 			code.insert( this.start, before );
 			code.move( this.body.start, this.body.end, this.start );
 			code.insert( this.start, after );
 
-			code.insert( this.end, `forLoop( ${args.join( ', ' )} );` );
+			if ( this.canBreak || this.canReturn ) {
+				const returned = functionScope.createIdentifier( 'returned' );
+
+				let insert = `{\n${i1}var ${returned} = ${loop}( ${args.join( ', ' )} );\n`;
+				if ( this.canBreak ) insert += `\n${i1}if ( ${returned} === 'break' ) break;`;
+				if ( this.canReturn ) insert += `\n${i1}if ( ${returned} ) return returned.v;`;
+				insert += `\n${i0}}`;
+
+				code.insert( this.end, insert );
+			} else {
+				code.insert( this.end, `${loop}( ${args.join( ', ' )} );` );
+			}
 		}
 
 		super.transpile( code );

src/program/types/index.js
@@ -1,10 +1,12 @@
 import ArrayExpression from './ArrayExpression.js';
 import ArrowFunctionExpression from './ArrowFunctionExpression.js';
 import AssignmentExpression from './AssignmentExpression.js';
+import BreakStatement from './BreakStatement.js';
 import CallExpression from './CallExpression.js';
 import ClassBody from './ClassBody.js';
 import ClassDeclaration from './ClassDeclaration.js';
 import ClassExpression from './ClassExpression.js';
+import ContinueStatement from './ContinueStatement.js';
 import ExportDefaultDeclaration from './ExportDefaultDeclaration.js';
 import ForStatement from './ForStatement.js';
 import ForOfStatement from './ForOfStatement.js';
@@ -15,6 +17,7 @@ import ImportDefaultSpecifier from './ImportDefaultSpecifier.js';
 import Literal from './Literal.js';
 import Identifier from './Identifier.js';
 import Property from './Property.js';
+import ReturnStatement from './ReturnStatement.js';
 import Super from './Super.js';
 import TaggedTemplateExpression from './TaggedTemplateExpression.js';
 import TemplateElement from './TemplateElement.js';
@@ -28,10 +31,12 @@ export default {
 	ArrayExpression,
 	ArrowFunctionExpression,
 	AssignmentExpression,
+	BreakStatement,
 	CallExpression,
 	ClassBody,
 	ClassDeclaration,
 	ClassExpression,
+	ContinueStatement,
 	ExportDefaultDeclaration,
 	ForStatement,
 	ForOfStatement,
@@ -42,6 +47,7 @@ export default {
 	Literal,
 	Identifier,
 	Property,
+	ReturnStatement,
 	Super,
 	TaggedTemplateExpression,
 	TemplateElement,

test/samples/block-scoping.js
@@ -87,14 +87,14 @@ module.exports = [
 				console.log( square );
 			}
 
-			var forLoop = function ( i ) {
+			var loop = function ( i ) {
 				var square = i * i;
 				setTimeout( function () {
 					log( square );
 				}, i * 100 );
 			};
 
-			for ( var i = 0; i < 10; i += 1 ) forLoop( i );`
+			for ( var i = 0; i < 10; i += 1 ) loop( i );`
 	},
 
 	{
@@ -104,11 +104,11 @@ module.exports = [
 			for ( let i = 0; i < 10; i += 1 ) setTimeout( () => console.log( i ), i * 100 );`,
 
 		output: `
-			var forLoop = function ( i ) {
+			var loop = function ( i ) {
 				setTimeout( function () { return console.log( i ); }, i * 100 );
 			};
 
-			for ( var i = 0; i < 10; i += 1 ) forLoop( i );`
+			for ( var i = 0; i < 10; i += 1 ) loop( i );`
 	},
 
 	{
@@ -476,14 +476,14 @@ module.exports = [
 			var arguments$1 = arguments;
 			var this$1 = this;
 
-			var forLoop = function ( i ) {
+			var loop = function ( i ) {
 				console.log( this$1, arguments$1, i );
 				setTimeout( function () {
 					console.log( this, arguments, i );
 				}, i * 100 );
 			};
 
-			for ( var i = 0; i < 10; i += 1 ) forLoop( i );`
+			for ( var i = 0; i < 10; i += 1 ) loop( i );`
 	},
 
 	{
@@ -500,14 +500,14 @@ module.exports = [
 		output: `
 			var fns = [];
 
-			var forLoop = function ( i$1 ) {
+			var loop = function ( i$1 ) {
 				fns.push(function () { return i$1; });
 				i$1 += 1;
 
 				i = i$1;
 			};
 
-			for ( var i = 0; i < 10; i += 1 ) forLoop( i );`
+			for ( var i = 0; i < 10; i += 1 ) loop( i );`
 	},
 
 	{
@@ -526,17 +526,18 @@ module.exports = [
 			var i = 'conflicting';
 			var fns = [];
 
-			var forLoop = function ( i$2 ) {
+			var loop = function ( i$2 ) {
 				fns.push(function () { return i$2; });
 				i$2 += 1;
 
 				i$1 = i$2;
 			};
 
-			for ( var i$1 = 0; i$1 < 10; i$1 += 1 ) forLoop( i$1 );`
+			for ( var i$1 = 0; i$1 < 10; i$1 += 1 ) loop( i$1 );`
 	},
 
 	{
+		solo: true,
 		description: 'handles break and continue inside block-scoped loops (#12)',
 
 		input: `
@@ -550,16 +551,16 @@ module.exports = [
 			}`,
 
 		output: `
-			function () {
-				var forLoop = function ( i ) {
+			function foo () {
+				var loop = function ( i ) {
 					if ( i % 2 ) return;
 					if ( i > 5 ) return 'break';
 					if ( i === 'potato' ) return { v: 'huh?' };
 					setTimeout( function () { return console.log( i ); } );
 				};
 
-				for ( let i = 0; i < 10; i += 1 ) {
-					var returned = forLoop( i );
+				for ( var i = 0; i < 10; i += 1 ) {
+					var returned = loop( i );
 
 					if ( returned === 'break' ) break;
 					if ( returned ) return returned.v;

commit 17554604bb2999d1c33d752ce70d4b142148ec41

fix bugs introduced with #12 fix

src/program/types/ReturnStatement.js
@@ -3,15 +3,22 @@ import Node from '../Node.js';
 export default class ReturnStatement extends Node {
 	initialise () {
 		this.loop = this.findNearest( /(?:For|While)Statement/ );
-		if ( this.loop ) this.loop.canReturn = true;
+		this.nearestFunction = this.findNearest( /Function/ );
+
+		if ( this.loop && ( !this.nearestFunction || this.loop.depth > this.nearestFunction.depth ) ) {
+			this.loop.canReturn = true;
+			this.shouldWrap = true;
+		}
+
+		this.argument.initialise();
 	}
 
 	transpile ( code ) {
-		if ( this.loop && this.loop.shouldRewriteAsFunction ) {
-			code.insert( this.argument.start, `{ v: ` );
-			code.insert( this.argument.end, ` }` );
-		}
+		const shouldWrap = this.shouldWrap && this.loop && this.loop.shouldRewriteAsFunction;
+		if ( shouldWrap ) code.insert( this.argument.start, `{ v: ` );
 
-		super.transpile( code );
+		this.argument.transpile( code );
+
+		if ( shouldWrap ) code.insert( this.argument.end, ` }` );
 	}
 }